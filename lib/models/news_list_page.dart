8594289899b09f1b5797144990e825a79216c8dd
Inna Kotlovianova <Inna.Kotlovanova@gmail.com>
2023-12-12 19:25:33 +0100

    Initial commit

---
+++ lib/models/news_list_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NewsListPage extends StatefulWidget {
+  const NewsListPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewsListPage> createState() => _NewsListPageState();
+}
+
+class _NewsListPageState extends State<NewsListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Latest news'),
+      ),
+    );
+  }
+}
