2afef19141f934fdf81c6885b666fa251ff0dea2
Inna Kotlovianova <Inna.Kotlovanova@gmail.com>
2023-12-14 11:05:24 +0100

    Add model api

---
+++ lib/api/api_request.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:news_list01/models/news_models.dart';
+import 'package:http/http.dart' as http;
+
+class ApiRequest {
+  Future<List<NewsModels>> selectNews() async {
+    final http.Response response = await http.get(
+      Uri.parse(
+        'http://api.mediastack.com/v1/news?access_key=ddf6da89f3961874766b013a67ab0fd6',
+      ),
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['data']
+          .map<NewsModels>((item) => NewsModels.fromJson(item))
+          .toList();
+    } else{
+      throw Exception('No news found');
+    }
+  }
+}
